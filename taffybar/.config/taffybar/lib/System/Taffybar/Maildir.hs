c17849cf5876db184572073e4d9dfb717efc34aa
Igor Babuschkin <igor@babuschk.in>
2014-01-24 16:20:11 +0100

    Implement simple mail info popup

--- taffybar/.config/taffybar/lib/System/Taffybar/Maildir.hs
+++ taffybar/.config/taffybar/lib/System/Taffybar/Maildir.hs
@@ -1,42 +1,79 @@
 module System.Taffybar.Maildir ( mailDirNew ) where
 
-import Text.Regex.Posix
 import System.INotify
 import System.Directory
+import System.FilePath.Posix
+import Data.List
+import Control.Monad
 import Control.Concurrent
+import Control.Concurrent.MVar
+import qualified Data.Set as S
+import Text.Regex.Posix
 
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Display.Image
 import Graphics.UI.Gtk.Layout.HBox
 
-isNewMail s | s == "."  = False
-            | s == ".." = False
-            | s =~ ",[^,]*S[^,]*$" = False
+import System.Taffybar.Widgets.Util
+
+makeMailList :: IO Window
+makeMailList = do
+  container <- windowNew
+  return container
+
+toggleMailList :: WidgetClass w => w -> Window -> IO Bool
+toggleMailList w c = do
+  visible <- get c widgetVisible
+  if visible then widgetHideAll c
+             else do
+               attachPopup w "Mails" c
+               displayPopup w c
+  return True
+
+getAbsDirectoryContents :: FilePath -> IO [FilePath]
+getAbsDirectoryContents dir = do
+  contents <- getDirectoryContents dir
+  return $ map (dir </>) contents
+
+isNewMail s | takeFileName s =~ ".*\\.$" = False
+            | takeFileName s =~ ",[^,]*S[^,]*$" = False
             | otherwise = True
 
-mailCallback name path label _ = do
-  contNew <- getDirectoryContents (path ++ "/new")
-  contCur <- getDirectoryContents (path ++ "/cur")
-  let cont = contNew ++ contCur
-      num = length . filter isNewMail $ cont
-  case num of
+createTooltipText newMail = do
+  conts <- forM newMail readFile
+  info <- forM conts $ \x -> do
+      let from = x =~ "From: (.*)$" !! 0 !! 1
+          subject = x =~ "Subject: (.*)$" !! 0 !! 1
+      return $ from ++ ": " ++ subject
+  return $ unlines info
+
+mailCallback name path label tips _ = do
+  contNew <- getAbsDirectoryContents (path </> "new")
+  contCur <- getAbsDirectoryContents (path </> "cur")
+  let newMail = filter isNewMail $ contNew ++ contCur
+  let count = length newMail
+
+  case count of
     0 -> do
       postGUIAsync $ widgetHideAll label
     _ -> do
-      labelSetMarkup label (name ++ ": " ++ show num)
+      text <- createTooltipText newMail
+      --tooltipsSetTip tips label text text
+      labelSetMarkup label (name ++ ": " ++ show count)
       postGUIAsync $ widgetShowAll label
 
-initMaildir name path label = do
-  mailCallback name path label ()
+initMaildir name path label tips = do
+  mailCallback name path label tips ()
   inot <- initINotify
   watchSubDir inot "/new"
   watchSubDir inot "/cur"
   return ()
-    where watchSubDir i p = addWatch i [MoveIn] (path ++ p) (mailCallback name path label)
+    where watchSubDir i p = addWatch i [MoveIn] (path ++ p) (mailCallback name path label tips)
 
 mailDirNew name path = do
   label <- labelNew Nothing
-  on label realize $ initMaildir name path label
+  tips <- tooltipsNew
+  on label realize $ initMaildir name path label tips
   --box <- hBoxNew False 2
   --image <- imageNewFromFile "/home/igor/test.png"
   --boxPackStart box image PackGrow 0
@@ -44,3 +81,5 @@ mailDirNew name path = do
   --widgetShowAll box
   widgetShowAll label
   return $ toWidget label
+
+
