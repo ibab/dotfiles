0408f1b3690f5e5989f58eeeb7587c0d46c5fb27
Igor Babuschkin <igor@babuschk.in>
2014-05-08 16:46:43 +0200

    Improve promtpt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,4 +1,8 @@
 
+termtitle="%n@%m:%~"
+if [ $TERM != "linux" ]; then
+    print -Pn "\e]2;$termtitle\a"
+fi
 
 if [[ "$TERM" =~ ".*-256color" ]]; then
     local primary="113"
@@ -8,12 +12,13 @@ if [[ "$TERM" =~ ".*-256color" ]]; then
     local light="237"
 
     jobsprompt="%(1j.%F{yellow}[%j]%f.)"
-    #promptmarker="%K{$light}%F{$normal}%K{$light}%k%F{$light}%f"
-    promptmarker="%F{$dark}%B%#%f%b"
+    #promptmarker="%K{$dark}%F{$black}%F{113} %#%F{$dark}%k%f"
+    promptmarker="%F{$primary}%B%#%f%b"
     foldersegment="%F{$dark}%K{$dark}%F{$primary}%~ %F{$black}%f%k"
     usernamesegment="%K{$black}%F{$primary}%n%f%k"
-    sshsegment="%F{223}@%m%f"
-    vcssegment=" %K{$black} %c%u %F{$primary}%f%K{$primary} %F{$dark}%b%F{$dark} "
+    sshsegment="%F{221}@%m%f"
+    vcssegment=" %K{$black} %c%u %F{$primary}%f%K{$primary}%F{$dark} %b%F{$dark} "
+    vcsactionsegment="%K{$black} %c%u %F{$primary}%K{$primary}%F{$dark} %a %F{$dark} %b%F{$dark} "
 
     PS2="%K{$light} %k  "
 else
@@ -23,6 +28,7 @@ else
     usernamesegment="%{%F{green}%}(%n)%f"
     sshsegment="%{%F{yellow}%}(%m)%f"
     vcssegment="%c%u %F{2}%b %f"
+    vcsactionsegment="%c%u %f{3}[%a]%f%f{2}(%b)%f"
     PS2="| "
 fi
 
@@ -31,6 +37,9 @@ PS1="$jobsprompt$promptmarker "
 function precmd {
     vcs_info
     RPS1="${vcs_info_msg_0_}$foldersegment"
+    if [ $TERM != "linux" ]; then
+        print -Pn "\e]2;$termtitle\a" 2>/dev/null
+    fi
 }
 
 # Add yellow marker when connected over SSH
@@ -50,12 +59,12 @@ fi
 
 ## vcs functions (for dynamic right prompt)
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git hg
+zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr '%F{2}M%f'
 zstyle ':vcs_info:git:*' unstagedstr '%F{3}M%f'
 zstyle ':vcs_info:git:*' formats "$vcssegment"
-zstyle ':vcs_info:git:*' actionformats '%c%u %F{3}[%a]%f%F{2}(%b)%f'
+zstyle ':vcs_info:git:*' actionformats "$vcsactionsegment"
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
 function +vi-git-untracked(){
